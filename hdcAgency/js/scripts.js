f1ca145f187662ff3f7b146342ebd061ea7b698a
LucasF <lucas.feiteira@outlook.com>
2025-03-27 14:56:11 -0300

    .

--- hdcAgency/js/scripts.js
+++ hdcAgency/js/scripts.js
@@ -112,11 +112,37 @@ $(document).ready(function(){
     
 
     // Filtro do portfólio
-    $('.filtro-btn').on('click', function () {
+    $('.filter-btn').on('click', function () {
 
         let type = $(this).attr('id');
         let boxes = $('.project-box');
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+        if (type == 'dsg-btn') {
+            eachBoxes('dsg', boxes)
+        } else if (type == 'dev-btn') {
+            eachBoxes('dev', boxes);
+        } else if (type == 'seo-btn') {
+            eachBoxes('seo', boxes);
+        } else {
+            eachBoxes('all', boxes);
+        }
         
     });
    
+    function eachBoxes(type, boxes) {
+        if (type == 'all') {
+            $(boxes).fadeIn();
+        } else {
+            $(boxes).each(function () {
+               if (!$(this).hasClass(type)) {
+                   $(this).fadeOut('slow');
+               } else {
+                   $(this).fadeIn();
+               }
+            });
+        }
+    }
 });
