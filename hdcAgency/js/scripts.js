a1e1fed7c05eaf15e7e251ee60ca8dd4f1bcc093
LucasF <lucas.feiteira@outlook.com>
2025-03-31 18:33:26 -0300

    .

--- hdcAgency/js/scripts.js
+++ hdcAgency/js/scripts.js
@@ -145,4 +145,41 @@ $(document).ready(function(){
             });
         }
     }
+
+    //scroll para secoes
+
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+        
+        let btnId = $(this).attr('id');
+
+        if (btnId == 'mainSlider') {
+            scrollTo = aboutSection;
+        } else if (btnId == 'about-area') {
+            scrollTo = servicesSection;
+        } else if (btnId == 'team-area') {
+            scrollTo = teamSection;
+        } else if (btnId == 'portfolio-area') {
+            scrollTo = portfolioSection;
+        } else if (btnId =='contact-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1500);
+    });
+
 });
